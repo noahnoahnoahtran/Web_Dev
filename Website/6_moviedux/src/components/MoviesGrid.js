ae08eb46a41c12561214af5a8ac0334607761a4d
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-01-20 23:46:37 +0700

    load movies from json file

--- Website/6_moviedux/src/components/MoviesGrid.js
+++ Website/6_moviedux/src/components/MoviesGrid.js
@@ -4,8 +4,9 @@ import "../styles.css";
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    const m = ["a", "b", "c", "d"];
-    setMovies(m);
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
   }, []);
   return <div>{movies.length}</div>;
 }
