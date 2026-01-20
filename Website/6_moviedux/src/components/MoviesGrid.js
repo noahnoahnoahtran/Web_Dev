11d83a2330269da20c75535d486b03d757765bcd
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-01-20 23:54:36 +0700

    render movies

--- Website/6_moviedux/src/components/MoviesGrid.js
+++ Website/6_moviedux/src/components/MoviesGrid.js
@@ -8,5 +8,18 @@ export default function MoviesGrid() {
       .then((response) => response.json())
       .then((data) => setMovies(data));
   }, []);
-  return <div>{movies.length}</div>;
+  return (
+    <div className="movies-grid">
+      {movies.map((movie) => (
+        <div key={movie.id} className="movie-card">
+          <img src={`images/${movie.image}`} alt={movie.title}></img>
+          <div className="movie-card-info">
+            <h3 className="movie-card-title">{movie.title}</h3>
+            <p className="movie-card-genre">{movie.genre}</p>
+            <p className="movie-card-rating">{movie.rating}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
