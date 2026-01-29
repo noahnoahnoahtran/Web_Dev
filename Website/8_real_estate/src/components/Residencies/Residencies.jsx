e363645a90af7ebf2d8f43e701e32034fc86987e
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-01-29 17:08:48 +0700

    feat(residencies): import .json slider data

--- Website/8_real_estate/src/components/Residencies/Residencies.jsx
+++ Website/8_real_estate/src/components/Residencies/Residencies.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "./Residencies.css";
 import "swiper/css";
+import data from "../../utils/slider.json";
 
 function Residencies() {
-    return();
+  return <div>Residencies</div>;
 }
 
-export default Residencies
+export default Residencies;
