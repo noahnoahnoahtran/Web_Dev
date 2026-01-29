0114c48eb224f93baeed186b705159f7ce476cc0
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-01-29 17:04:44 +0700

    chore(residencies): install swiper dependency

--- Website/8_real_estate/src/App.jsx
+++ Website/8_real_estate/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Hero></Hero>
       </div>
       <Companies />
+      <Residencies />
     </div>
   );
 }
---
+++ Website/8_real_estate/src/components/Residencies/Residencies.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "./Residencies.css";
+import "swiper/css";
+
+function Residencies() {
+    return();
+}
+
+export default Residencies
