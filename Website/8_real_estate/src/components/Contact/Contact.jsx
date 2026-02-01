5016e31d1a7f606d7b32286a1c6a6235c0a18d76
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-01 17:01:04 +0700

    feat(contact): structure contact component (general)

--- Website/8_real_estate/src/components/Contact/Contact.jsx
+++ Website/8_real_estate/src/components/Contact/Contact.jsx
@@ -2,7 +2,27 @@ import React, { useState } from "react";
 import "./Contact.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        {/* left side */}
+        <div className=" flexColStart c-left">
+          <span className="orangeText">Our Contacts</span>
+          <span className="primaryText">Easy to Contact us</span>
+          <span className="secondaryText">
+            We are always ready to help by providing the best services for you.
+            We believe a good place to live can make your life better
+          </span>
+        </div>
+        {/* right side */}
+        <div className="c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
--- Website/8_real_estate/src/components/Value/Value.jsx
+++ Website/8_real_estate/src/components/Value/Value.jsx
@@ -54,7 +54,7 @@ function Value() {
           <span className="orangeText">Our Value</span>
           <span className="primaryText">Value We Give to You</span>
           <span className="secondaryText">
-            We always ready to help by providing the best services for you.
+            We are always ready to help by providing the best services for you.
             <br />
             We believe a good place to live can make your life better
           </span>
