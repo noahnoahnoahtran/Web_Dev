93cdb6b1303e6d9497c456fef1ddb730eebf7c74
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-03 19:18:19 +0700

    feat(header): add transition and outside click effect to menu icon

--- Website/8_real_estate/src/components/header/header.jsx
+++ Website/8_real_estate/src/components/header/header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Header.css";
 import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
 
 function Header() {
   const [menuOpened, setMenuOpened] = useState();
@@ -13,15 +14,22 @@ function Header() {
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="logo" width={100}></img>
-        <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
-          <a href="">Residencies</a>
-          <a href="">Our Values</a>
-          <a href="">Contact Us</a>
-          <a href="">Get Started</a>
-          <button className="button">
-            <a href="">Contact</a>
-          </button>
-        </div>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setMenuOpened(false);
+          }}
+        >
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+            <a href="">Residencies</a>
+            <a href="">Our Values</a>
+            <a href="">Contact Us</a>
+            <a href="">Get Started</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
+
         <div
           className="menu-icon"
           onClick={() => setMenuOpened((prev) => !prev)}
