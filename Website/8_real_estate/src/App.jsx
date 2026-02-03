a1a1cd15478621925bf797e2ee8c9aa092891208
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-03 16:47:49 +0700

    chore(footer): create component and import libs

--- Website/8_real_estate/src/App.jsx
+++ Website/8_real_estate/src/App.jsx
@@ -5,6 +5,7 @@ import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
 import GetStarted from "./components/GetStarted/GetStarted";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
       <Value />
       <Contact />
       <GetStarted />
+      <Footer />
     </div>
   );
 }
---
+++ Website/8_real_estate/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import "./Footer.css";
+
+function Footer() {
+  return <div>Footer</div>;
+}
+
+export default Footer;
