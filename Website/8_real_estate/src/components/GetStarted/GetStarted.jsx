1363fb5a96ffcf2fd2b43ee70c70f2f4170755b0
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-03 16:38:00 +0700

    feat(getstarted): structure get started component

--- Website/8_real_estate/src/components/GetStarted/GetStarted.jsx
+++ Website/8_real_estate/src/components/GetStarted/GetStarted.jsx
@@ -2,7 +2,20 @@ import React, { useState } from "react";
 import "./GetStarted.css";
 
 function GetStarted() {
-  return <div>Get Started</div>;
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get Started with Homyz</span>
+          <span className="secondaryText">
+            Subscribe and find super attractive price quotes from us.
+            <br />
+            Find your residence soon
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default GetStarted;
