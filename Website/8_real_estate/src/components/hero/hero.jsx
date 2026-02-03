3dcd4906eeda56ec4ff731a29070fb4c226cf70c
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-03 22:07:59 +0700

    style(hero): add animation to title

--- Website/8_real_estate/src/components/hero/hero.jsx
+++ Website/8_real_estate/src/components/hero/hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 function Hero() {
   return (
@@ -10,11 +11,15 @@ function Hero() {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 2, type: "spring" }}
+            >
               Discover <br />
               Most Suitable <br />
               Property
-            </h1>
+            </motion.h1>
           </div>
           <div className="flexColStart hero-des">
             <span className="secondaryText">
