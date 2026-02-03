ea206d350b8a665cd0b648e1afcf3fa6d865ff9b
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-03 18:55:21 +0700

    style(header): add styling to menu icon

--- Website/8_real_estate/src/components/header/header.jsx
+++ Website/8_real_estate/src/components/header/header.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { BiMenuAltRight } from "react-icons/bi";
 
 function Header() {
+  const [menuOpened, setMenuOpened] = useState();
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
@@ -16,9 +17,9 @@ function Header() {
             <a href="">Contact</a>
           </button>
         </div>
-      </div>
-      <div className="menu-icon">
-        <BiMenuAltRight size={30} />
+        <div className="menu-icon">
+          <BiMenuAltRight size={30} />
+        </div>
       </div>
     </section>
   );
