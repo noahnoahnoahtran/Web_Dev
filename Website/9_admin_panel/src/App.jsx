452f9334ea2ba0ff281210f2e12e5ad176572019
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-04 17:42:22 +0700

    chore(sidebar): structure component and import libs

--- Website/9_admin_panel/src/App.jsx
+++ Website/9_admin_panel/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
         Testinggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg
+        <Sidebar />
       </div>
     </div>
   );
