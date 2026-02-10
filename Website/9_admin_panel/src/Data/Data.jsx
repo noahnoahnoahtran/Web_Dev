687f7b33cbd0ddf224669a6a6a4a1c9b41e772ee
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-10 17:27:58 +0700

    feat(data): add cards data

--- Website/9_admin_panel/src/Data/Data.jsx
+++ Website/9_admin_panel/src/Data/Data.jsx
@@ -31,3 +31,56 @@ export const SidebarData = [
     heading: "Analytics",
   },
 ];
+
+// Cards data
+export const CardsData = [
+  {
+    title: "Sales",
+    color: {
+      background: "linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)",
+      boxshadow: "0px 10px 20px 0px #e0c6f5",
+    },
+    barvalue: 70,
+    value: "25,970",
+    png: UilUsdSquare,
+    series: [
+      {
+        name: "Sales",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+    ],
+  },
+  {
+    title: "Revenue",
+    color: {
+      background: "linear-gradient(180deg, #ff919d 0%, #fc929d 100%)",
+      boxshadow: "0px 10px 20px 0px #fdc0c7",
+    },
+    barvalue: 80,
+    value: "14,270",
+    png: UilMoneyWithdrawal,
+    series: [
+      {
+        name: "Revenue",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      },
+    ],
+  },
+  {
+    title: "Expenses",
+    color: {
+      background:
+        "linear-gradient(rgb(248, 212, 154) - 146.42%, rgb(255 202 113) -46.42%)",
+      boxshadow: "0px 10px 20px 0px #f9d59b",
+    },
+    barvalue: 60,
+    value: "4,270",
+    png: UilClipboardAlt,
+    series: [
+      {
+        name: "Expenses",
+        data: [10, 25, 15, 30, 12, 15, 20],
+      },
+    ],
+  },
+];
