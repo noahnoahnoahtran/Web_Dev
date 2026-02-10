73663fe72fb6b3fa8a543309948d509113e5de3f
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-10 22:21:57 +0700

    chore(card): structure component and import libs

---
+++ Website/9_admin_panel/src/components/Card/Card.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import "./Card.css";
+
+function Card() {
+  return <div></div>;
+}
+
+export default Card;
--- Website/9_admin_panel/src/components/Cards/Cards.jsx
+++ Website/9_admin_panel/src/components/Cards/Cards.jsx
@@ -1,8 +1,26 @@
 import React, { useState } from "react";
 import "./Cards.css";
+import { CardsData } from "../../Data/Data";
 
 function Cards() {
-  return <div></div>;
+  return (
+    <div className="cards">
+      {CardsData.map((card, id) => {
+        return (
+          <div className="parentContainer">
+            <Card
+              title={card.title}
+              color={card.color}
+              barValue={card.barvalue}
+              value={card.value}
+              png={card.png}
+              series={card.series}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Cards;
