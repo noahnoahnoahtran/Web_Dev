f9dec8773be66bbc99fcf1c0fa6c0afc701214a7
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-15 13:58:09 +0700

    feat(navbar): create html structure

--- Website/10_portfolio/src/layout/Navbar.jsx
+++ Website/10_portfolio/src/layout/Navbar.jsx
@@ -1,3 +1,29 @@
+const navLinks = [
+  { href: "#about", label: "About" },
+  { href: "#projects", label: "Projects" },
+  { href: "#experience", label: "Experience" },
+  { href: "#testimonials", label: "Testimonials" },
+];
+
 export const Navbar = () => {
-  return <header></header>;
+  return (
+    <header>
+      <nav>
+        {/* Logo */}
+        <a>
+          NT<span>.</span>
+        </a>
+        {/* Desktop Nav*/}
+        <div>
+          <div>
+            {navLinks.map((link, index) => (
+              <a href={link.href} key={index}>
+                {link.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
