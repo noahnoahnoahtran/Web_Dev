958a412b730e652f280fd39f1a6a25c16b0cfbaa
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-16 18:03:36 +0700

    feat(hero): structure html

--- Website/10_portfolio/src/sections/Hero.jsx
+++ Website/10_portfolio/src/sections/Hero.jsx
@@ -1,3 +1,29 @@
 export const Hero = () => {
-  return <section></section>;
+  return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+      {/* Background */}
+      <div className="absolute inset-0">
+        <img
+          src="/hero-bg.png"
+          alt="Hero image"
+          className="w-full h-full object-cover opacity-40"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background"></div>
+      </div>
+
+      {/* Blue Dots*/}
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        {[...Array(30)].map((_, i) => (
+          <div
+            className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+            style={{
+              backgroundColor: "#60a5fa",
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+            }}
+          ></div>
+        ))}
+      </div>
+    </section>
+  );
 };
