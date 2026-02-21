4662ee9441ce433e9ad91fe6efbe55b9de1ed0ef
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-21 16:50:49 +0700

    feat(footer): setup structure and add styling

--- Website/10_portfolio/src/App.jsx
+++ Website/10_portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import { Projects } from "@/sections/Projects";
 import { Experience } from "@/sections/Experience";
 import { Testimonials } from "@/sections/Testimonials";
 import { Contact } from "@/sections/Contact";
+import { Footer } from "@/layout/Footer";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Experience />
         <Testimonials />
         <Contact />
+        <Footer />
       </main>
     </div>
   );
