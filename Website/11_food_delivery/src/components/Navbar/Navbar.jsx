ed2c6c07b581ff140f8bbdad206192b1ade4f20e
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-23 01:16:59 +0700

    feat(navbar): apply hooks to make menu responsive

--- Website/11_food_delivery/src/components/Navbar/Navbar.jsx
+++ Website/11_food_delivery/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
 const Navbar = () => {
-  const [menu];
+  const [menu, setMenu] = useState("home");
 
   return (
     <div className="navbar">
       <img src={assets.logo} alt="logo" className="logo" />
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search icon" />
@@ -20,7 +40,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="basket icon" />
           <div className="dot"></div>
         </div>
-        <button>Sign In</button>
+        <button>sign in</button>
       </div>
     </div>
   );
