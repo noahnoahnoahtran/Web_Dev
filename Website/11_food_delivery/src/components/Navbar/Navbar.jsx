c7b90227fda83e8198831057de4b2e55f62964dd
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-23 00:59:40 +0700

    feat(navbar): setup html structure

--- Website/11_food_delivery/src/components/Navbar/Navbar.jsx
+++ Website/11_food_delivery/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Navbar.css";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="logo" className="logo" />
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile App</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search icon" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="basket icon" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
