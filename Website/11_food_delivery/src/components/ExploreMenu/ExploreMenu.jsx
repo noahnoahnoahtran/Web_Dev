b1998ea2139259a4523d4468aefef8d191b6afda
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-25 23:47:12 +0700

    feat(home/exploremenu): setup state hooks to determine menu selection

--- Website/11_food_delivery/src/components/ExploreMenu/ExploreMenu.jsx
+++ Website/11_food_delivery/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
@@ -13,8 +13,20 @@ const ExploreMenu = () => {
       </p>
       <div className="explore-menu-list">
         {menu_list.map((item, index) => (
-          <div key={index} className="explore-menu-list-item">
-            <img src={item.menu_image} alt={item.menu_name} />
+          <div
+            onClick={() =>
+              setCategory((prev) =>
+                prev === item.menu_name ? "All" : item.menu_name,
+              )
+            }
+            key={index}
+            className="explore-menu-list-item"
+          >
+            <img
+              className={category === item.menu_name ? "active" : ""}
+              src={item.menu_image}
+              alt={item.menu_name}
+            />
             <p>{item.menu_name}</p>
           </div>
         ))}
