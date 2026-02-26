940c8cc18cac674069f0dc728f9fad8ba0c2aa6e
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-27 00:29:20 +0700

    feat(home/fooddisplay): setup html structure and create fooditem component

---
+++ Website/11_food_delivery/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FoodItem.css";
+
+const FoodItem = ({ id, name, price, description, image }) => {
+  return <div>FoodItem</div>;
+};
+
+export default FoodItem;
