b0ba203000fef538412000e462cc5bcc06fbbade
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-02-27 23:03:36 +0700

    style(fooddisplay): add css styling

--- Website/11_food_delivery/src/components/FoodDisplay/FoodDisplay.jsx
+++ Website/11_food_delivery/src/components/FoodDisplay/FoodDisplay.jsx
@@ -6,7 +6,7 @@ import FoodItem from "../FoodItem/FoodItem";
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
   return (
-    <div classname="food-display" id="food-display">
+    <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => (
