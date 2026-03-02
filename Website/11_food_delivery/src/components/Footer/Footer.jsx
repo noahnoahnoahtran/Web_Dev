c042cb0a0bec1a45eedbcf011b8bb00de110c377
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-02 23:25:26 +0700

    feat(footer): setup html structure

---
+++ Website/11_food_delivery/src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="logo" />
+          <p>abcd</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="facebook icon" />
+            <img src={assets.twitter_icon} alt="twitter icon" />
+            <img src={assets.linkedin_icon} alt="linkedin icon" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com - All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
