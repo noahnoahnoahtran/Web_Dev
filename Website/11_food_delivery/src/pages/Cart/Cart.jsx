f750a3842566014cc9773497eda3decb3b301a7b
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-07 15:36:36 +0700

    feat(cart): setup remove functionality

--- Website/11_food_delivery/src/pages/Cart/Cart.jsx
+++ Website/11_food_delivery/src/pages/Cart/Cart.jsx
@@ -24,10 +24,12 @@ const Cart = () => {
                 <div className="cart-items-title cart-items-item">
                   <img src={item.image} alt="food image" />
                   <p>{item.name}</p>
-                  <p>{item.price}</p>
+                  <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>{item.price * cartItems[item._id]}</p>
-                  <p className="cross">x</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
                 </div>
                 <hr />
               </div>
