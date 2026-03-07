1f6c4a512afc9b1d3c38abaa8a9c01e2d552c053
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-07 10:22:16 +0700

    feat(login): setup state hooks for login popup

---
+++ Website/11_food_delivery/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./LoginPopup.css"
+
+const LoginPopup = () => {
+  return (
+    <div className="login-popup">LoginPopup</div>
+  )
+}
+
+export default LoginPopup
