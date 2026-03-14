3e3b4fcaee14bcd16971ea76c3da478a0fe42462
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-14 23:10:32 +0700

    chore(constants): create constants

--- Website/12_brainwave/src/App.jsx
+++ Website/12_brainwave/src/App.jsx
@@ -1,14 +1,15 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
+        {/* <Button className="mt-10" href="#login">
           Something
-        </Button>
+        </Button> */}
+        <Header />
       </div>
       <ButtonGradient />
     </>
